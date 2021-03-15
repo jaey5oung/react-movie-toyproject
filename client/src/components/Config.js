dd9c99e83ea475cb64baf07469f82194746deabe
장재영 <jaey5oung@naver.com>
2021-03-15 19:35:26 +0900

    3/15 mainlmage,gridCard 제작

---
+++ client/src/components/Config.js
@@ -0,0 +1,7 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/'
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
+export const API_KEY = 'e5f46659cd7fc7a9a33393e8cae7ebf1'
+//이런식으로 상수를 만드는이유는 앞에의 URL은 동일하기때문에 좀 더 쉽게쓰기위해서이다.
