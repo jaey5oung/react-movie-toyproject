083d487ea6abcfd087135deef1b31f1049b17f3a
장재영 <jaey5oung@naver.com>
2021-03-16 18:02:08 +0900

    3/16 detailpage 제작

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -8,15 +8,29 @@ import { Row } from 'antd';
 function LandingPage() {
   const [Movies, setMovies] = useState([]);
   const [MainMovieImage, setMainMovieImage] = useState(null);
+  const [CurrentPage, setCurrentPage] = useState(0);
+
   useEffect(() => {
     const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    fetchMovies(endpoint);
+  }, []);
+
+  const fetchMovies = (endpoint) => {
     fetch(endpoint)
       .then((response) => response.json())
       .then((response) => {
-        setMovies(...[response.results]);
+        setMovies([...Movies, ...response.results]);
         setMainMovieImage(response.results[0]);
+        setCurrentPage(response.page);
       });
-  }, []);
+  };
+
+  const loadMoreItems = () => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${
+      CurrentPage + 1
+    }`;
+    fetchMovies(endpoint);
+  };
 
   return (
     <div style={{ width: '100%', margin: '0' }}>
@@ -31,13 +45,14 @@ function LandingPage() {
       <div style={{ width: '85%', margin: '1rem auto' }}>
         <h2>Movie by latest</h2>
         <hr />
-        
+
         {/* movie grid cards */}
-        <Row gutter={[18,18]}>
+        <Row gutter={[16, 16]}>
           {Movies &&
             Movies.map((movie, index) => (
               <React.Fragment key={index}>
                 <GridCards
+                  landingPage
                   image={movie.poster_path ? `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
                   movieId={movie.id}
                   movieName={movie.original_title}
@@ -47,7 +62,7 @@ function LandingPage() {
         </Row>
       </div>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <button> Load More </button>
+        <button onClick={loadMoreItems}> Load More </button>
       </div>
     </div>
   );
---
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
+import { json } from 'body-parser';
+import MainImage from '../LandingPage/Sections/MainImage';
+import MovieInfo from './Sections/MovieInfo';
+import GridCards from '../commons/GridCards';
+import { Row } from 'antd';
+
+function MovieDetail(props) {
+  let movieId = props.match.params.movieId;
+  const [Movie, setMovie] = useState([]);
+  const [Casts, setCasts] = useState([]);
+
+  useEffect(() => {
+    let endpointCrew = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
+    let endpointInfo = `${API_URL}movie/${movieId}?api_key=${API_KEY}`;
+
+    fetch(endpointInfo)
+      .then((response) => response.json())
+      .then((result) => {
+        setMovie(result);
+      });
+
+    fetch(endpointCrew)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log('responseForCrew', response);
+        setCasts(response.cast);
+      });
+  }, []);
+  return (
+    <div>
+      {/* Header */}
+      <MainImage
+        image={`${IMAGE_BASE_URL}w1280${Movie.backdrop_path}`}
+        title={Movie.original_title}
+        text={Movie.overview}
+      />
+
+      {/* Body */}
+      <div style={{ width: '85%', margin: '1rem auto' }}>
+        {/* Movie Info */}
+
+        <MovieInfo movie={Movie} />
+
+        <br />
+        {/* Actors Grid */}
+
+        <Row gutter={[16, 16]}>
+          {Casts &&
+            Casts.map((cast, index) => (
+              <React.Fragment key={index}>
+                <GridCards
+                  image={cast.profile_path ? `${IMAGE_BASE_URL}w500${cast.proflie_path}` : null}
+                  characterName={cast.name}
+                />
+              </React.Fragment>
+            ))}
+        </Row>
+
+        <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem' }}></div>
+        <button>Toggle Actor View</button>
+      </div>
+    </div>
+  );
+}
+
+export default MovieDetail;
